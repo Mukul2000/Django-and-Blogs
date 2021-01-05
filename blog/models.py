9315dfb6b47e35a7cdf12865cbb971942ca80721
Mukul <mukulkumar627@gmail.com>
2021-01-05 23:23:44 +0530

    Added model for comments section and applied migrations to database. Modified Post detail page to show all comments beneath the post.

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,32 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
-     title = models.CharField(max_length = 1600)
-     content = models.TextField()
-     date_posted = models.DateTimeField(default=timezone.now)
-     #we are not using timezone.now() because we dont want 
-     #to call the function, we are just passing a reference
-     #of the function
-     author = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length=1600)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # we are not using timezone.now() because we dont want
+    # to call the function, we are just passing a reference
+    # of the function
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-     def __str__(self):
-          return self.title
+    def __str__(self):
+        return self.title
 
-     def get_absolute_url(self):
-          return reverse('post-detail', kwargs = {'pk':self.pk})
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    # cascade deletes all comments if a post is deleted.
+    # sort of foreign key that connects a comment to a post
+
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+         return f'{self.post.title}-{self.name}'
